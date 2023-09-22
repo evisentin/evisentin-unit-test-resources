13f95294c4c549d7208aa367550678ae52dfbe84
Enrico Visentin <visentin.enrico@gmail.com>
2023-09-22 14:24:16 +0200

    added step03-spring-wiremock

--- step03-spring-wiremock/src/main/java/ch/ev/unit/test/resources/step03/spring/repositories/rest/StudentRestRepository.java
+++ step03-spring-wiremock/src/main/java/ch/ev/unit/test/resources/step03/spring/repositories/rest/StudentRestRepository.java
@@ -3,46 +3,62 @@ package ch.ev.unit.test.resources.step03.spring.repositories.rest;
 import ch.ev.unit.test.resources.step03.spring.data.Student;
 import ch.ev.unit.test.resources.step03.spring.exceptions.RestException;
 import ch.ev.unit.test.resources.step03.spring.repositories.StudentRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.DefaultUriBuilderFactory;
 
-import java.net.URI;
+import java.io.IOException;
 import java.util.Optional;
+
+import static org.apache.commons.lang3.StringUtils.removeEnd;
 
 @Slf4j
 @Repository
 public class StudentRestRepository implements StudentRepository {
 
-    private final RestTemplate restTemplate;
-    private final DefaultUriBuilderFactory uriBuilderFactory;
+    private final String serviceUrl;
+    private final OkHttpClient httpClient;
+    private final ObjectMapper objectMapper;
 
     public StudentRestRepository(
             @Value("${app.services.external.student.base-url}") final @NonNull String serviceUrl,
-            final RestTemplate restTemplate) {
+            final OkHttpClient httpClient, final ObjectMapper objectMapper) {
 
-        uriBuilderFactory = new DefaultUriBuilderFactory(serviceUrl);
-        this.restTemplate = restTemplate;
+        this.serviceUrl = removeEnd(serviceUrl, "/");
+        this.httpClient = httpClient;
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public Optional<Student> getById(final @NonNull Long id) {
 
-        URI uri = uriBuilderFactory.builder().pathSegment("api", "student", "{id}").build(id);
+        final String url = serviceUrl + "/api/student/" + id;
+        final Request request = new Request.Builder().url(url).build();
 
-        final ResponseEntity<Student> response = restTemplate.getForEntity(uri, Student.class);
+        try (final Response response = httpClient.newCall(request).execute()) {
 
-        if (response.getStatusCode().is2xxSuccessful())
-            return Optional.ofNullable(response.getBody());
+            if (response.isSuccessful()) {
+                return asStudent(response);
+            }
 
-        if (response.getStatusCode().value() == HttpStatus.NOT_FOUND.value())
-            return Optional.empty();
+            if (response.code() == 404) return Optional.empty();
 
-        throw new RestException(response.getStatusCode());
+            throw new RestException(response.code());
+
+        } catch (IOException e) {
+            throw new RestException(e);
+        }
+    }
+
+    private Optional<Student> asStudent(final @NonNull Response response) throws IOException {
+        final ResponseBody body = response.body();
+        if (body == null) return Optional.empty();
+        return Optional.ofNullable(objectMapper.readValue(body.bytes(), Student.class));
     }
 }
